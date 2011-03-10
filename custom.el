225a36f32e3034ff72028d82f8d78f2772267ff5
David Porter <david.porter@anu.edu.au>
2011-03-10 13:10:37 +1100

    Improve faces for ruby-complexity-mode

--- custom.el
+++ custom.el
@@ -9,4 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ruby-complexity-complexity-high ((t (:foreground "SlateGray4"))))
+ '(ruby-complexity-complexity-low ((t (:foreground "gray80"))))
+ '(ruby-complexity-complexity-normal ((t (:foreground "gray20")))))
