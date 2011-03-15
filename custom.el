26386d5db26baa0ddc8d8566c53aa65666a8593c
David Porter <david.a.porter@gmail.com>
2011-03-15 17:32:40 +1100

    Don't underline current line
    Also, remove ruby-complexity customised faces now we're using zenburn.

--- custom.el
+++ custom.el
@@ -9,6 +9,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ruby-complexity-complexity-high ((t (:foreground "SlateGray4"))))
- '(ruby-complexity-complexity-low ((t (:foreground "gray80"))))
- '(ruby-complexity-complexity-normal ((t (:foreground "gray20")))))
+ '(highlight ((t (:inherit zenburn-background-1 :underline nil)))))
