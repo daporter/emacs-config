e524eeef70f95bacdb4b13682079d00fe3c765d6
David Porter <david.a.porter@gmail.com>
2012-07-02 16:17:29 +1000

    Add springboard

--- init.el
+++ init.el
@@ -623,52 +623,53 @@
             (use-package ido-hacks
               :init (ido-hacks-mode 1))
 
-    (defun ido-smart-select-text ()
-      "Select the current completed item.  Do NOT descend into directories."
-      (interactive)
-      (when (and (or (not ido-require-match)
-                     (if (memq ido-require-match
-                               '(confirm confirm-after-completion))
-                         (if (or (eq ido-cur-item 'dir)
-                                 (eq last-command this-command))
-                             t
-                           (setq ido-show-confirm-message t)
-                           nil))
-                     (ido-existing-item-p))
-                 (not ido-incomplete-regexp))
-        (when ido-current-directory
-          (setq ido-exit 'takeprompt)
-          (unless (and ido-text (= 0 (length ido-text)))
-            (let ((match (ido-name (car ido-matches))))
-              (throw 'ido
-                     (setq ido-selected
-                           (if match
-                               (replace-regexp-in-string "/\\'" "" match)
-                             ido-text)
-                           ido-text ido-selected
-                           ido-final-text ido-text)))))
-        (exit-minibuffer)))
-    
-    (add-hook 'ido-minibuffer-setup-hook
-              #'(lambda ()
-                  (bind-key "<return>" 'ido-smart-select-text
-                            ido-file-completion-map)))
-    
-    (defun ido-switch-buffer-tiny-frame (buffer)
-      (interactive (list (ido-read-buffer "Buffer: " nil t)))
-      (with-selected-frame
-          (make-frame '((width                . 80)
-                        (height               . 22)
-                        (left-fringe          . 0)
-                        (right-fringe         . 0)
-                        (vertical-scroll-bars . nil)
-                        (unsplittable         . t)
-                        (has-modeline-p       . nil)
-                        (minibuffer           . nil)))
-        (switch-to-buffer buffer)
-        (set (make-local-variable 'mode-line-format) nil)))
-    
-    (bind-key "C-x 5 t" 'ido-switch-buffer-tiny-frame)))
+            (use-package ido-springboard)
+            (defun ido-smart-select-text ()
+              "Select the current completed item.  Do NOT descend into directories."
+              (interactive)
+              (when (and (or (not ido-require-match)
+                             (if (memq ido-require-match
+                                       '(confirm confirm-after-completion))
+                                 (if (or (eq ido-cur-item 'dir)
+                                         (eq last-command this-command))
+                                     t
+                                   (setq ido-show-confirm-message t)
+                                   nil))
+                             (ido-existing-item-p))
+                         (not ido-incomplete-regexp))
+                (when ido-current-directory
+                  (setq ido-exit 'takeprompt)
+                  (unless (and ido-text (= 0 (length ido-text)))
+                    (let ((match (ido-name (car ido-matches))))
+                      (throw 'ido
+                             (setq ido-selected
+                                   (if match
+                                       (replace-regexp-in-string "/\\'" "" match)
+                                     ido-text)
+                                   ido-text ido-selected
+                                   ido-final-text ido-text)))))
+                (exit-minibuffer)))
+
+            (add-hook 'ido-minibuffer-setup-hook
+                      #'(lambda ()
+                          (bind-key "<return>" 'ido-smart-select-text
+                                    ido-file-completion-map)))
+
+            (defun ido-switch-buffer-tiny-frame (buffer)
+              (interactive (list (ido-read-buffer "Buffer: " nil t)))
+              (with-selected-frame
+                  (make-frame '((width                . 80)
+                                (height               . 22)
+                                (left-fringe          . 0)
+                                (right-fringe         . 0)
+                                (vertical-scroll-bars . nil)
+                                (unsplittable         . t)
+                                (has-modeline-p       . nil)
+                                (minibuffer           . nil)))
+                (switch-to-buffer buffer)
+                (set (make-local-variable 'mode-line-format) nil)))
+
+            (bind-key "C-x 5 t" 'ido-switch-buffer-tiny-frame)))
 
 ;;;_ , Ledger
 
@@ -800,7 +801,7 @@
                    (if (= ?/ (aref dired-directory last-idx))
                        (substring dired-directory 0 last-idx)
                      dired-directory)))))
-          
+
           (add-hook 'dired-mode-hook 'recentf-add-dired-directory)))
 
 ;;;_ , Enhanced-Ruby-Mode
@@ -919,7 +920,7 @@
   :if (not noninteractive)
   :init (progn
           (winner-mode 1)
-          
+
           (bind-key "M-N" 'winner-redo)
           (bind-key "M-P" 'winner-undo)))
 
@@ -943,7 +944,7 @@
              (expand-file-name "snippets/" user-emacs-directory))
 
             (bind-key "<tab>" 'yas/next-field-or-maybe-expand yas/keymap)
-            
+
             (defun yas/new-snippet (&optional choose-instead-of-guess)
               (interactive "P")
               (let ((guessed-directories (yas/guess-snippet-directories)))
