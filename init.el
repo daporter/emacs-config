8fb0a760f4a8ca9aaaf005791072460dcf6505aa
David Porter <david.a.porter@gmail.com>
2012-07-10 16:44:30 +1000

    Simplify ido config

--- init.el
+++ init.el
@@ -626,64 +626,26 @@ Including indent-buffer, which should not be called automatically on save."
 ;;;_ , ido
 
 (use-package ido
-  :defines (ido-cur-item
-            ido-require-match
-            ido-selected
-            ido-final-text
-            ido-show-confirm-message)
-  :init (ido-mode 'buffer)
+  :init (progn
+          (ido-mode t)
 
-  :config (progn
-            (use-package ido-hacks
-              :init (ido-hacks-mode 1))
+          (setq ido-enable-prefix            nil
+                ido-enable-flex-matching     t
+                ido-create-new-buffer        'always
+                ido-use-filename-at-point    nil
+                ido-save-directory-list-file "~/.emacs.d/data/ido.last"
+                ido-max-prospects            10)
 
-            (use-package ido-springboard)
-            (defun ido-smart-select-text ()
-              "Select the current completed item.  Do NOT descend into directories."
-              (interactive)
-              (when (and (or (not ido-require-match)
-                             (if (memq ido-require-match
-                                       '(confirm confirm-after-completion))
-                                 (if (or (eq ido-cur-item 'dir)
-                                         (eq last-command this-command))
-                                     t
-                                   (setq ido-show-confirm-message t)
-                                   nil))
-                             (ido-existing-item-p))
-                         (not ido-incomplete-regexp))
-                (when ido-current-directory
-                  (setq ido-exit 'takeprompt)
-                  (unless (and ido-text (= 0 (length ido-text)))
-                    (let ((match (ido-name (car ido-matches))))
-                      (throw 'ido
-                             (setq ido-selected
-                                   (if match
-                                       (replace-regexp-in-string "/\\'" "" match)
-                                     ido-text)
-                                   ido-text ido-selected
-                                   ido-final-text ido-text)))))
-                (exit-minibuffer)))
+          ;; Always rescan buffer for imenu.
+          (set-default 'imenu-auto-rescan t)
 
-            (add-hook 'ido-minibuffer-setup-hook
-                      #'(lambda ()
-                          (bind-key "<return>" 'ido-smart-select-text
-                                    ido-file-completion-map)))
+          (bind-key "C-w" 'ido-delete-backward-updir
+                    ido-file-completion-map)
+          (bind-key "C-x C-w" 'ido-copy-current-file-name
+                    ido-file-completion-map)
 
-            (defun ido-switch-buffer-tiny-frame (buffer)
-              (interactive (list (ido-read-buffer "Buffer: " nil t)))
-              (with-selected-frame
-                  (make-frame '((width                . 80)
-                                (height               . 22)
-                                (left-fringe          . 0)
-                                (right-fringe         . 0)
-                                (vertical-scroll-bars . nil)
-                                (unsplittable         . t)
-                                (has-modeline-p       . nil)
-                                (minibuffer           . nil)))
-                (switch-to-buffer buffer)
-                (set (make-local-variable 'mode-line-format) nil)))
-
-            (bind-key "C-x 5 t" 'ido-switch-buffer-tiny-frame)))
+          (use-package ido-ubiquitous
+            :init (ido-ubiquitous-mode 1))))
 
 ;;;_ , jump-char
 
