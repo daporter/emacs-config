45ec09896c3ea8f47e1b61c9f7a35d7436d158fe
David Porter <david.a.porter@gmail.com>
2012-07-11 13:56:40 +1000

    Fix yasnippet config

--- init.el
+++ init.el
@@ -970,49 +970,24 @@ Including indent-buffer, which should not be called automatically on save."
 ;;;_ , yasnippet
 
 (use-package yasnippet
-  :if (not noninteractive)
-  :diminish yas/minor-mode
+  :mode     ("/\\.emacs\\.d/snippets/" . snippet-mode)
   :commands (yas/minor-mode yas/expand)
-  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
-  :init (hook-into-modes #'(lambda () (yas/minor-mode 1))
-                         '(prog-mode-hook
-                           org-mode-hook
-                           ruby-mode-hook
-                           message-mode-hook
-                           gud-mode-hook
-                           erc-mode-hook))
+
+  :init (progn
+          (hook-into-modes #'(lambda () (yas/minor-mode 1))
+                           '(prog-mode-hook ruby-mode-hook)))
+
   :config (progn
+            (setq yas/snippet-dirs     "~/.emacs.d/snippets"
+                  yas/trigger-key      (kbd "C-c y TAB")
+                  yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))
+
+            (bind-key "TAB" 'yas/next-field-or-maybe-expand yas/keymap)
+            (bind-key "RET" 'yas/exit-all-snippets          yas/keymap)
+
             (yas/initialize)
-            (yas/load-directory
-             (expand-file-name "snippets/" user-emacs-directory))
-
-            (bind-key "<tab>" 'yas/next-field-or-maybe-expand yas/keymap)
-
-            (defun yas/new-snippet (&optional choose-instead-of-guess)
-              (interactive "P")
-              (let ((guessed-directories (yas/guess-snippet-directories)))
-                (switch-to-buffer "*new snippet*")
-                (erase-buffer)
-                (kill-all-local-variables)
-                (snippet-mode)
-                (set (make-local-variable 'yas/guessed-modes)
-                     (mapcar #'(lambda (d)
-                                 (intern (yas/table-name (car d))))
-                             guessed-directories))
-                (unless (and choose-instead-of-guess
-                             (not (y-or-n-p
-                                   "Insert a snippet with useful headers? ")))
-                  (yas/expand-snippet "\
-# -*- mode: snippet -*-
-# name: $1
-# --
-$0"))))
-
-            (bind-key "C-c y TAB" 'yas/expand)
-            (bind-key "C-c y n" 'yas/new-snippet)
-            (bind-key "C-c y f" 'yas/find-snippets)
-            (bind-key "C-c y r" 'yas/reload-all)
-            (bind-key "C-c y v" 'yas/visit-snippet-file)))
+            ;; Use only my own snippets, not the bundled ones.
+            (yas/load-directory yas/snippet-dirs)))
 
 ;;;_ , zenburn-theme
 
