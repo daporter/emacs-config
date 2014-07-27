5f32452ba73d73f7e7c36d38611740ce76ed4e8f
David Porter <david.a.porter@gmail.com>
2014-07-27 16:57:53 +1000

    Add bindings for haskell-mode as per the docs

--- init.el
+++ init.el
@@ -1102,7 +1102,16 @@ Including indent-buffer, which should not be called automatically on save."
 
           (turn-on-haskell-doc-mode)
           (turn-on-haskell-indentation)
-          (turn-on-haskell-decl-scan)))
+          (turn-on-haskell-decl-scan)
+
+          (define-key haskell-mode-map (kbd "C-x C-d") nil)
+          (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+          (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+          (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+          (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+          (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+          (define-key haskell-mode-map (kbd "C-c M-.") nil)
+          (define-key haskell-mode-map (kbd "C-c C-d") nil)))
 
 (require 'ediff)
 ;; Use ediff in single-frame mode.
