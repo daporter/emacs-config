ce13cd4011f3b71d3c9de59382aca5eea88e9d77
David Porter <david.a.porter@gmail.com>
2014-08-11 12:07:36 +1000

    Turn off smartparens-global-mode

--- init.el
+++ init.el
@@ -251,9 +251,9 @@ non-directory part only."
 (setq make-pointer-invisible 1)
 
 (require 'desktop)
-(desktop-save-mode 1)
 (setq desktop-restore-eager 10)
 (setq desktop-dirname dap/user-data-directory)
+(desktop-save-mode 1)
 
 (use-package uniquify
   :config (setq uniquify-buffer-name-style 'forward))
@@ -1135,9 +1135,7 @@ Including indent-buffer, which should not be called automatically on save."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(magit-use-overlays nil)
- '(smartparens-global-mode t))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
