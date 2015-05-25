c7af8c3e2c15484c8c042fe4f2e0ecf445bd8a3c
David Porter <david.a.porter@gmail.com>
2015-05-25 15:38:46 +1000

    Don't save copy of outgoing mail
    Gmail will save a copy for us automatically.

--- init.el
+++ init.el
@@ -965,8 +965,7 @@ Including indent-buffer, which should not be called automatically on save."
 
             (setq notmuch-archive-tags '("-inbox" "-unread" "+archive"))
 
-            (setq notmuch-fcc-dirs '(("david.porter@anu.edu.au" . "anu/sent")
-                                     (".*" . nil)))
+            (setq notmuch-fcc-dirs nil)
 
             (setq notmuch-saved-searches
                   '((:name "anu unread" :query "tag:anu AND tag:unread" :sort-order oldest-first)
