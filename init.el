7bc4611b461cb02d70ec54a362fd2ef88eea4926
David Porter <david.a.porter@gmail.com>
2015-05-28 07:19:09 +1000

    Update package list

--- init.el
+++ init.el
@@ -1342,6 +1342,9 @@ Including indent-buffer, which should not be called automatically on save."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
  '(magit-use-overlays nil)
  '(package-selected-packages
    (quote
