7b1de0c6d4115d6a2146e6bf386f62e8ae5cd38d
David Porter <david.a.porter@gmail.com>
2015-09-07 09:05:27 +1000

    Squashed 'site-lisp/notmuch/' changes from 005c2f0..bd5504e
    bd5504e lib: constify argument to notmuch_database_status_string
    9ee29ad test: move backup_database and restore_database to library
    110694b lib: note remaining uses of deprecated message search API
    f16944c lib: remove use of notmuch_query_search_messages from query.cc
    
    git-subtree-dir: site-lisp/notmuch
    git-subtree-split: bd5504ec10edf6a1442d92774dc06fafc0b46e3f

--- lib/database.cc
+++ lib/database.cc
@@ -1441,6 +1441,8 @@ notmuch_database_upgrade (notmuch_database_t *notmuch,
 	notmuch_message_t *message;
 	char *filename;
 
+	/* XXX: this should use the _st version, but needs an error
+	   path */
 	for (messages = notmuch_query_search_messages (query);
 	     notmuch_messages_valid (messages);
 	     notmuch_messages_move_to_next (messages))
@@ -2651,7 +2653,7 @@ notmuch_database_get_all_tags (notmuch_database_t *db)
 }
 
 const char *
-notmuch_database_status_string (notmuch_database_t *notmuch)
+notmuch_database_status_string (const notmuch_database_t *notmuch)
 {
     return notmuch->status_string;
 }
