ba5f8b68196c59bf7719eac5d1a768db616abcb9
David Porter <david.a.porter@gmail.com>
2017-12-09 10:32:56 +1100

    Use package `counsel-projectile'

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-fill fill smartparens smartparens-config zenburn-theme ace-window which-key try counsel swiper markdown-mode+ git-commit-mode magit))))
+    (counsel-projectile auto-fill fill smartparens smartparens-config zenburn-theme ace-window which-key try counsel swiper markdown-mode+ git-commit-mode magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
