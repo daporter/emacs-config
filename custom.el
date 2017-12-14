283500d2e6dc719a80b8322ca4c4611b36c29fbe
David Porter <david.a.porter@gmail.com>
2017-12-14 11:33:19 +1100

    Use package `ws-butler'

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (god-mode powerline solarized-theme keyfreq undo-tree counsel-projectile auto-fill fill smartparens smartparens-config zenburn-theme ace-window which-key try counsel swiper markdown-mode+ git-commit-mode magit)))
+    (yaml-mode twittering-mode chruby define-word org-journal company exec-path-from-shell org-bullets ws-butler minitest god-mode powerline boon solarized-theme xah-fly-keys keyfreq undo-tree counsel-projectile auto-fill fill smartparens smartparens-config zenburn-theme ace-window which-key try counsel swiper markdown-mode+ git-commit-mode magit)))
  '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
