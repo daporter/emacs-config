d1a9c82327474b957d2ab53025da1335f4a67bb1
David Porter <david.a.porter@gmail.com>
2017-12-27 13:47:18 +1000

    Gnus tweaks

--- gnus.el
+++ gnus.el
@@ -1,20 +1,27 @@
-(setq gnus-select-method '(nntp "news.gmane.org"))
+;; No primary server:
+(setq gnus-select-method '(nnnil ""))
 
-(add-to-list 'gnus-secondary-select-methods
-             '(nnimap "gmail"
-                      (nnimap-address "imap.gmail.com")
-                      (nnimap-server-port "imaps")
-                      (nnimap-stream ssl)
-                      ;; Move expired messages to Gmail's trash.
-                      (nnmail-expiry-target "nnimap+gmail:[Gmail]/Trash")
-                      ;; Mails marked as expired can be processed immediately.
-                      (nnmail-expiry-wait immediate)))
+(setq gnus-secondary-select-methods
+      '((nnimap "gmail"
+                (nnimap-address "imap.gmail.com")
+                (nnimap-server-port "imaps")
+                (nnimap-stream ssl)
+                ;; Move expired messages to Gmail's trash.
+                (nnmail-expiry-target "nnimap+gmail:[Gmail]/Trash")
+                ;; Mails marked as expired can be processed immediately.
+                (nnmail-expiry-wait immediate))
+        (nntp "gmane"
+              (nntp-address "news.gmane.org"))))
 
 (setq smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587)
 
 ;; Make Gnus NOT ignore [Gmail] mailboxes
 (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+;; Archive outgoing email in Sent folder on imap.gmail.com:
+(setq gnus-message-archive-method '(nnimap "gmail")
+      gnus-message-archive-group "[Gmail]/Sent Mail")
 
 ;; Don't render HTML-mails but show the text part if it's available.
 (with-eval-after-load "mm-decode"
@@ -35,9 +42,9 @@
 
 (setq gnus-ignored-headers "^References:\\|^Xref:")
 
-(setq gnus-sum-thread-tree-single-indent   "◇ "
-      gnus-sum-thread-tree-root            "◆ "
-      gnus-sum-thread-tree-false-root      ""
+(setq gnus-sum-thread-tree-single-indent   " "
+      gnus-sum-thread-tree-root            "◆"
+      gnus-sum-thread-tree-false-root      "◇"
       gnus-sum-thread-tree-vertical        "│ "
       gnus-sum-thread-tree-leaf-with-other "├─►"
       gnus-sum-thread-tree-single-leaf     "└─►"
@@ -54,8 +61,10 @@
         (t                              . "%B %d '%y")))
 
 (setq gnus-summary-line-format
-      (concat "%U%R %~(pad-right 2)t%* %B %~(max-right 30)~(pad-right 30)n  "
+      (concat "%U%R %~(pad-left 2)t%*  %B %~(max-right 30)~(pad-right 30)n  "
               "%~(max-right 90)~(pad-right 90)s %-135=%&user-date;\n"))
+
+(setq gnus-article-date-headers '(local))
 
 (setq gnus-parameters
       '(("nnimap+gmail:[Gmail]/goldencheetah"
