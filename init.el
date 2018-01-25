f5e2ea42fe983c9bb8c377bd373c9d282c150d1b
David Porter <david.a.porter@gmail.com>
2018-01-25 18:02:58 +1100

    Add a Flycheck checker for proselint

--- init.el
+++ init.el
@@ -304,7 +304,19 @@
   :disabled)
 
 (use-package flycheck
-  :config (global-flycheck-mode 1))
+  :config (progn
+            (flycheck-define-checker proselint
+              "A linter for prose."
+              :command ("proselint" source-inplace)
+              :error-patterns ((warning
+                                line-start
+                                (file-name) ":" line ":" column ": "
+                                (id (one-or-more (not (any " "))))
+                                (message) line-end))
+              :modes (text-mode markdown-mode gfm-mode message-mode))
+
+            (add-to-list 'flycheck-checkers 'proselint)
+            (global-flycheck-mode 1)))
 
 (use-package avy-flycheck
   :after (avy flycheck)
